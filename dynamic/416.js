4c203834e1d76703bc391f00edafede26d80d60a
liu-mengwei <542221757@qq.com>
2020-07-08 00:05:57 +0800

    416优化

--- dynamic/416.js
+++ dynamic/416.js
@@ -32,4 +32,102 @@ var canPartition = function (nums) {
   return deep(nums.length - 1, sum) === sum
 };
 
-canPartition([1, 5, 11, 5]);
+
+// 实际上返回布尔值就行了
+var canPartition = function (nums) {
+  let sum = 0;
+  nums.forEach(item => {
+    sum += item;
+  })
+  if (sum % 2 > 0) return false;
+  sum = sum / 2;
+
+  let cache = {};
+
+  function deep(index, sum) {
+    if (index < 0 || sum < 0) {
+      return false;
+    }
+
+    if (sum === 0) {
+      return true;
+    }
+
+    if (typeof cache[`${index} ${sum}`] === 'boolean') {
+      return cache[`${index} ${sum}`];
+    }
+
+    let ret = deep(index - 1, sum) || deep(index - 1, sum - nums[index]);
+    cache[`${index} ${sum}`] = ret;
+    return ret;
+  }
+
+  return deep(nums.length - 1, sum);
+};
+
+// 动态规划解法
+var canPartition = function (nums) {
+  let sum = 0;
+  nums.forEach(item => {
+    sum += item;
+  })
+  if (sum % 2 > 0) return false;
+  sum = sum / 2;
+
+
+  let dp = Array.from(new Array(nums.length), () => new Array(sum + 1));
+  for (let j = 0; j <= sum; j++) {
+    dp[0][j] = nums[0] === j;
+  }
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j <= sum; j++) {
+      dp[i][j] = dp[i - 1][j];
+
+      if (j === nums[i]) {
+        dp[i][j] = true;
+        continue;
+      }
+
+      if (j > nums[i]) {
+        dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i]];
+      }
+    }
+  }
+
+  return dp[nums.length - 1][sum];
+};
+
+
+// 动态规划解法 降维优化
+var canPartition2 = function (nums) {
+  if (!nums.length) return true;
+  let sum = 0;
+  nums.forEach(item => {
+    sum += item;
+  })
+  if (sum % 2 > 0) return false;
+  sum = sum / 2;
+
+
+  let dp = new Array(sum + 1).fill(false);
+  dp[nums[0]] = true;
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = sum; j >= 0; j--) {
+
+      if (j === nums[i]) {
+        dp[j] = true;
+        continue;
+      }
+
+      if (j > nums[i]) {
+        dp[j] = dp[j] || dp[j - nums[i]];
+      }
+    }
+  }
+
+  return dp[sum];
+};
+
+canPartition2([1, 5, 11, 5]);
